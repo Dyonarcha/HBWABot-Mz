eff9658960c240d9a57cdb762711a210809810de
Dyonarcha <159687028+Dyonarcha@users.noreply.github.com>
2024-03-03 18:29:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,8 +29,8 @@ global.prefa = ['','!','.','#','/']
 global.sessionName = 'session'
 
 // Sticker Pack Name
-global.packname = "Sticker By"
-global.author = "HBWABot"
+global.packname = "Sticker By"꧁Kazuki꧂
+global.author = "꧁Kazuki꧂"
 global.hbmods = "https://herbert70.blogspot.com/"
 //_______________________________________________
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
